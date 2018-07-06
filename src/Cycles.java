19ee95cbd4327e685c1052d8036dffcdd2b6265d
Andrei Ponomarenko <3355844@gmail.com>
2018-07-06 19:42:45 +0300

    Initial commit

---
+++ src/Cycles.java
@@ -0,0 +1,24 @@
+public class Cycles {
+    public void whileCycle(int i) {
+        System.out.println("While cycle");
+        while (i <= 10) {// true
+            System.out.println("value: " + i);
+            i++;
+        }
+        i = 0;
+        System.out.println("While cycle (true)& break");
+        while (true) {
+            if (i > 10) break;//false
+            System.out.println("value: " + i);
+            i++;
+        }
+        System.out.println("Do while cycle");
+        i = 0;
+        do {
+            if (i == 0) break;
+            System.out.println("value: " + i);
+            i++;
+        } while (i <= 10);//true
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Cycles сycle = new Cycles();
+        сycle.whileCycle(0);
+    }
+}
